5ddcc4763b5f9d2b293186a0071132b25ecd27d2
Nathan Bell <nathanbell@Nathans-MacBook-Pro.local>
2020-12-04 14:44:11 -0600

    gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ninjaGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
